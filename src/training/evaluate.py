06f9d746edd2cafe85975e1c7801e4b5299b1639
Siddhanth Sharma <siddhanth232509@gmail.com>
2026-01-22 22:45:33 +0530

    Evalutation file in added and been executed

--- src/training/evaluate.py
+++ src/training/evaluate.py
@@ -72,7 +72,7 @@ class BinaryEvaluator:
                 targets = targets.float().to(self.device)
                 
                 # Forward pass
-                logits = self.model(images).squeeze()
+                logits = self.model(images).view(-1)
                 probs = torch.sigmoid(logits)
                 preds = (probs > 0.5).long()
                 
